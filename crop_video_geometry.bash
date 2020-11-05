681e9f3a89eb528aafe013dda3211cd371351675
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-11-05 10:17:14 +0000

    Added geometry cropper

---
+++ crop_video_geometry.bash
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+input_name=$1 
+outwidth=$2 # pixels
+outheight=$3
+x_offset=0
+y_offset=0
+
+if [ $# -ge 4 ]
+then
+	x_offset=$4
+fi
+
+if [ $# -ge 5 ]
+then
+	y_offset=$5
+fi
+
+suffix=${input_name##*.}
+filename=${input_name%%.*}
+
+fileout=$filename$outwidth$outheight$x_offset$y_offset.avi
+
+echo $#
+
+ffmpeg -i $input_name -filter:v "crop=$outwidth:$outheight:$x_offset:$y_offset" -an -vcodec h264 $fileout
